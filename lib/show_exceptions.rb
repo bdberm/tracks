1867d40848d5ce6c85584f6b49602dc60ecd631f
Benjamin Berman <bdberm@gmail.com>
2017-11-07 17:41:10 -0500

    add basic error display with message and top of stack trace

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,30 @@
 require 'erb'
+require 'rack'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue Exception => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    filepath="lib/templates/rescue.html.erb"
+    file = File.read(filepath)
+
+    error_render = ERB.new(file).result(binding)
+
+    ["500", {"Content-type" => "text/html"}, [error_render]]
   end
 
 end
