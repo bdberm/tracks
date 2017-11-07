42b4c79fdf3a76a555f84af3889887919d039a5f
Benjamin Berman <bdberm@gmail.com>
2017-11-07 11:14:44 -0500

    comments for route and router

--- lib/router.rb
+++ lib/router.rb
@@ -8,7 +8,8 @@ class Route
     @action_name = action_name
   end
 
-  # checks if pattern matches path and method matches request method
+  # return true if request path matches route regex pattern and
+  # req http method matches route method
   def matches?(req)
     path = req.path
     method = req.request_method
@@ -20,7 +21,8 @@ class Route
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
-
+    #match pulls out MatchData object with key value pairs matching
+    #the regexp expression
     match = @pattern.match(req.path)
     route_params = {}
     match.names.each do |key|
@@ -44,21 +46,24 @@ class Router
     @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
-  # evaluate the proc in the context of the instance
-  # for syntactic sugar :)
+  #draw routes using the methods dynamically created below for the four
+  #http methods
   def draw(&proc)
     self.instance_eval(&proc)
   end
 
-  # make each of these methods that
-  # when called add route
+  #make instance method for each of the four http methods
+  #method takes arguments for pattern, controller, and action_name
+  #creates new route with those three arguments and the http method
+  #i.e. get(Regexp.new("^/recipes$", RecipesController, :index)) will
+  #create a route for /recipes, with http method get that routes to
+  #the RecipesController and the action index within it
   [:get, :post, :put, :delete].each do |http_method|
     define_method(http_method) do |pattern, controller_class, action_name|
        add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
-  # should return the route that matches this request
   def match(req)
     @routes.each do |route|
       return route if route.matches?(req)
